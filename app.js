35edb345475ceba034257d0da2926161fe0004a1
SmellydogCoding <daniel.mcneil@outlook.com>
2016-11-11 11:49:01 -0500

    Moved Twitter calls to seperate module.  Implimented ES6 Arrow Syntax

--- app.js
+++ app.js
@@ -1,57 +1,9 @@
 'use strict';
 
-try {
-  require('./env.js');
-} catch(error) {
-  // no env file in production environment
-}
-
-var express = require('express');
-var Twitter = require('twitter');
- 
-var client = new Twitter({
-  consumer_key: process.env.KEY,
-  consumer_secret: process.env.SECRET,
-  access_token_key: process.env.TOKEN,
-  access_token_secret: process.env.TOKENSECRET
-});
- 
-var params = {screen_name: 'SmellyDogCoding',count: 5};
-var tweetList;
-var friendList;
-var myDMs;
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (tweets) {
-    tweetList = tweets;
-  } else if (error) {
-    console.log(error);
-  }
-});
-
-var friends = client.get('friends/list', params, function(error, friends, response) {
-  if (friends) {
-    friendList = friends;
-  } else if (error) {
-    console.log(error);
-  }
-});
-
-var dms = client.get('direct_messages', params, function(error, dms, response) {
-  if (dms) {
-    myDMs = dms;
-  } else if (error) {
-    console.log(error);
-  }
-});
-
-// // load environmental variables when working in local enviroment
-try {
-  require('./env.js');
-} catch(error) {
-  // no env file in production environment
-}
-
-var app = express();
+const express = require('express');
+const app = express();
+var port = process.env.PORT || 3000;
+const twitter = require('./public/js/twitter.js');
 
 // serve static files in the public directory
 app.use(express.static(__dirname + '/public'));
@@ -61,12 +13,17 @@ app.set('view engine','pug');
 app.set('views', __dirname + '/views');
 
 // routing
-app.get('/', function(request, response) {
-  console.dir(friendList.users);
-  response.render('index',{tweets: tweetList, friends: friendList.users});
+app.get('/', function(req, res, next) {
+  req.tweetList = twitter.tweets();
+  req.friendsList = twitter.friends();
+  req.messageList = twitter.messages();
+  next();
+}, function(req,res) {
+  // console.dir(req.messageList);
+  res.render('index',{tweets: req.tweetList, friends: req.friendsList.users, messages: req.messageList});
 });
 
 // server
-app.listen(process.env.PORT, function() {
-  console.log('frontend server is running on port ' + process.env.PORT);
+app.listen(port, function() {
+  console.log('frontend server is running on port ' + port);
 });
---
+++ public/js/twitter.js
@@ -0,0 +1,72 @@
+'use strict';
+
+try {
+  require('./env.js');
+} catch(error) {
+  // no env file in production environment
+}
+
+const Twitter = require('twitter');
+ 
+const client = new Twitter({
+  consumer_key: process.env.KEY,
+  consumer_secret: process.env.SECRET,
+  access_token_key: process.env.TOKEN,
+  access_token_secret: process.env.TOKENSECRET
+});
+ 
+const params = {
+  screen_name: 'SmellyDogCoding',
+  count: 5
+};
+
+const getTweets = () => {
+  var data;
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (tweets) {
+      data = tweets;
+    } else if (error) {
+      console.log(error);
+    }
+  });
+  while(data === undefined) {
+    require('deasync').runLoopOnce();
+  };
+  return data;
+};
+
+const getFriends = () => {
+  var data;
+  client.get('friends/list', params, function(error, friends, response) {
+    if (friends) {
+      data = friends;
+    } else if (error) {
+      console.log(error);
+    }
+  });
+  while(data === undefined) {
+    require('deasync').runLoopOnce();
+  };
+  return data;
+};
+
+const getMessages = () => {
+  var data;
+  client.get('direct_messages', params, function(error, messages, response) {
+    if (messages) {
+      data = messages;
+    } else if (error) {
+      console.log(error);
+    }
+  });
+  while(data === undefined) {
+    require('deasync').runLoopOnce();
+  };
+  return data;
+};
+
+module.exports = {
+  tweets: getTweets,
+  friends: getFriends,
+  messages: getMessages,
+};
