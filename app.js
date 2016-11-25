bb26e397a8fa12dc8e236abb9f114ad6086e58e4
SmellydogCoding <daniel.mcneil@outlook.com>
2016-11-25 14:32:09 -0500

    Created error page

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.get('/', function(req, res, next) {
   req.tweetList = twitter.tweets();
   req.friendsList = twitter.friends();
   req.messageList = twitter.messages();
-  console.dir(req.messageList);
   next();
 }, function(req,res) {
   res.render('index',{tweets: req.tweetList, friends: req.friendsList.users, messages: req.messageList});
@@ -32,6 +31,14 @@ app.get('/post/:tweet', function(req,res,next) {
 }, function(req,res) {
   res.json(req.tweetList)
 });
+
+app.use(function (req, res, next) {
+  res.status(404).render('error',{status: '404', text1: 'Oh hai!  Sorry I couldn\'t find that page that you were looking for.', text2: 'But if you want to click the back button and try something else that\'s cool too.'});
+})
+
+app.use(function (req, res, next) {
+  res.status(500).render('error',{status: '500', text1: 'Uh oh.  It looks like something went wrong.  Just wait a couple minutes and try again.', text2: 'If you keep ending up back here, please tell the website owner about it.'});
+})
 
 // server
 app.listen(port, function() {
